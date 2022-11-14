009cc970950f662f470c03aff9ff90e81176e8cc
Gaurav Agrawal <gaurav.agrawal@c.thycotic.com>
2022-11-14 08:33:47 -0500

    upgrade the terraform to v1.3.4 and GoLang to 1.19

--- datasource_secret.go
+++ datasource_secret.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 
 	"github.com/DelineaXPM/tss-sdk-go/v2/server"
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceSecretRead(d *schema.ResourceData, meta interface{}) error {
